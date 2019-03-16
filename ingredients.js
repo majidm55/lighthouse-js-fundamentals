8875b4df1d7959ddaebf03a7cd371803c8007eb3
MAJID <mohammadmajid55@gmail.com>
2019-03-16 08:37:50 +0000

    For and while and backward exercise

---
+++ ingredients.js
@@ -0,0 +1,14 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// while (i < 1) {
+//   console.log(ingredients);
+//   i++;
+// }
+
+for (i = 0; i < 1; i++) {
+  console.log(ingredients);
+}
+
+for (i = ingredients.length - 1; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
