1b0a036af96bcfb7ffe24e305b7ccfbb1d3227a7
MAJID <mohammadmajid55@gmail.com>
2019-03-16 07:47:06 +0000

    Adds looping exercises

---
+++ chorus-while.js
@@ -0,0 +1,11 @@
+var chorus = "Let's dance!";
+var repeat = 0;
+while (repeat < 10) {
+  console.log(chorus);
+  repeat++;
+if (repeat === 5) {
+  console.log("*change key*");
+  }
+}
+
+console.log("Until the sun comes up!");
